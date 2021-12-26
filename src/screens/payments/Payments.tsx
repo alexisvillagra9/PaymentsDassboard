e438e3ef7a54758d3075e2f565b5837aca794ec0
Alexis Villagra <alexisvillagra9@gmail.com>
2021-12-26 12:19:24 -0300

    Skeleton loading

--- src/screens/payments/Payments.tsx
+++ src/screens/payments/Payments.tsx
@@ -1,3 +1,8 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
 import React, { useEffect, useState } from "react";
 import { PaymentFilters } from "../../components/paymentFilters/PaymentFilters";
 import { PaymentList } from "../../components/paymentList/PaymentList";
@@ -43,6 +48,45 @@ export const Payments = () => {
     paymentInit();
   }, [setPaymentOperations]);
 
+  const skeletonList = (
+    <>
+      <List
+        className="skeleton-container"
+        sx={{ width: "100%", padding: 0, bgcolor: "background.paper" }}
+      >
+        {[...Array(8)].map((x, idx) => (
+          <ListItem divider={true} key={idx}>
+            <Skeleton
+              animation="wave"
+              variant="circular"
+              className="skeleton-payment-avatar"
+            />
+            <ListItemText
+              primary={
+                <Skeleton
+                  animation="wave"
+                  height={"1.8rem"}
+                  className="skeleton-title"
+                />
+              }
+              secondary={
+                <Skeleton
+                  animation="wave"
+                  height={"1.3rem"}
+                  className="skeleton-subtitle"
+                />
+              }
+            />
+            <Stack spacing={1} alignItems="flex-end" marginLeft="0.5rem">
+              <Skeleton animation="wave" width={"6rem"} height={"1.8rem"} />
+              <Skeleton animation="wave" width={"6rem"} height={"1.8rem"} />
+            </Stack>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+
   return (
     <div className="payment-container">
       <PaymentFilters
@@ -54,7 +98,7 @@ export const Payments = () => {
         getTotalAmount={getTotalAmount}
       ></PaymentFilters>
       {loading ? (
-        <>CARGANDO</>
+        skeletonList
       ) : (
         <PaymentList paymentOperations={paymentOperationsFilter}></PaymentList>
       )}
