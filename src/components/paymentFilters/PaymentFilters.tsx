b7b7b57b8b081fb1b60c5d3a0e9d3469a182ca7a
Alexis Villagra <alexisvillagra9@gmail.com>
2022-01-19 10:46:24 -0300

    Finish reset filters

--- src/components/paymentFilters/PaymentFilters.tsx
+++ src/components/paymentFilters/PaymentFilters.tsx
@@ -4,6 +4,7 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import DatePicker from "@mui/lab/DatePicker";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import { CircularProgress } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import Chip from "@mui/material/Chip";
@@ -156,6 +157,26 @@ export const PaymentFilters = ({
     );
   };
 
+  const resetFilters = async () => {
+    setLoading(true);
+    const payops = await getPaymentOperationsByFilter({
+      statuses: [EPaymentOperationStatus.Terminated],
+    }); // Init with terminated payments only
+
+    setPaymentOperations(payops);
+    setSelectedStatuses([
+      filterStatuses.find(
+        (po) => po.code === EPaymentOperationStatus.Terminated
+      )?.description || "",
+    ]);
+    setSelectedOrigins(
+      filterOrigins.map((po) => po.description).concat(["all-origins"])
+    );
+    setDateTo(null);
+    setDateFrom(null);
+    setLoading(false);
+  };
+
   useEffect(() => {
     if (initComp) {
       setSelectedStatuses([
@@ -173,192 +194,213 @@ export const PaymentFilters = ({
     <>
       <Paper className="paper-container" elevation={0}>
         <FormGroup sx={{ m: 1 }}>
-          <Stack direction="row" gap="0.5rem" flexWrap="wrap">
-            <Button
-              id="openOriginSelect"
-              onClick={() => setOpenOriginSelect(true)}
-              className="select-button"
-              disabled={!initComp}
-            >
-              Origen
-              {iconFilter()}
-            </Button>
-            <Select
-              multiple
-              input={<Input id="select-multiple-origin" />}
-              onClose={() => setOpenOriginSelect(false)}
-              onChange={handleOriginsSelect}
-              style={{ display: "none" }}
-              open={openOriginSelect}
-              value={selectedOrigins}
-              MenuProps={{
-                anchorEl: document.getElementById("openOriginSelect"),
-              }}
-            >
-              <MenuItem key={"ORI"} value={"all-origins"}>
-                <ListItemText primary={"Todos"} />
-                <Checkbox
-                  checked={selectedOrigins.includes("all-origins")}
-                  className="checkbox-filter"
-                  onChange={handleSelectAllOrigins}
-                />
-              </MenuItem>
-              <Divider />
-              {filterOrigins.map((filOri) => (
-                <MenuItem key={filOri.code} value={filOri.description}>
-                  <ListItemText primary={filOri.description} />
+          <Stack direction="row">
+            <Stack direction="row" gap="0.5rem" flexWrap="wrap" flex="1">
+              <Button
+                id="openOriginSelect"
+                onClick={() => setOpenOriginSelect(true)}
+                className="select-button"
+                disabled={!initComp}
+              >
+                Origen
+                {iconFilter()}
+              </Button>
+              <Select
+                multiple
+                input={<Input id="select-multiple-origin" />}
+                onClose={() => setOpenOriginSelect(false)}
+                onChange={handleOriginsSelect}
+                style={{ display: "none" }}
+                open={openOriginSelect}
+                value={selectedOrigins}
+                MenuProps={{
+                  anchorEl: document.getElementById("openOriginSelect"),
+                }}
+              >
+                <MenuItem key={"ORI"} value={"all-origins"}>
+                  <ListItemText primary={"Todos"} />
                   <Checkbox
-                    checked={selectedOrigins.includes(filOri.description)}
+                    checked={selectedOrigins.includes("all-origins")}
                     className="checkbox-filter"
+                    onChange={handleSelectAllOrigins}
                   />
                 </MenuItem>
-              ))}
-            </Select>
-            <Button
-              id="openStatusSelect"
-              onClick={() => setOpenStatusSelect(true)}
-              className="select-button"
-              disabled={!initComp}
-            >
-              Estado
-              {iconFilter()}
-            </Button>
-            <Select
-              multiple
-              input={<Input id="select-multiple-statuses" />}
-              onChange={handleStatusesSelect}
-              onClose={() => setOpenStatusSelect(false)}
-              style={{ display: "none" }}
-              open={openStatusSelect}
-              value={selectedStatuses}
-              MenuProps={{
-                anchorEl: document.getElementById("openStatusSelect"),
-              }}
-              // onChange={}
-            >
-              <MenuItem key={"STS"} value={"all-statuses"}>
-                <ListItemText primary={"Todos"} />
-                <Checkbox
-                  checked={selectedStatuses.includes("all-statuses")}
-                  className="checkbox-filter"
-                  onChange={handleSelectAllStatuses}
-                />
-              </MenuItem>
-              <Divider />
-              {filterStatuses.map((filSta) => (
-                <MenuItem key={filSta.code} value={filSta.description}>
-                  <ListItemText primary={filSta.description} />
+                <Divider />
+                {filterOrigins.map((filOri) => (
+                  <MenuItem key={filOri.code} value={filOri.description}>
+                    <ListItemText primary={filOri.description} />
+                    <Checkbox
+                      checked={selectedOrigins.includes(filOri.description)}
+                      className="checkbox-filter"
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+              <Button
+                id="openStatusSelect"
+                onClick={() => setOpenStatusSelect(true)}
+                className="select-button"
+                disabled={!initComp}
+              >
+                Estado
+                {iconFilter()}
+              </Button>
+              <Select
+                multiple
+                input={<Input id="select-multiple-statuses" />}
+                onChange={handleStatusesSelect}
+                onClose={() => setOpenStatusSelect(false)}
+                style={{ display: "none" }}
+                open={openStatusSelect}
+                value={selectedStatuses}
+                MenuProps={{
+                  anchorEl: document.getElementById("openStatusSelect"),
+                }}
+                // onChange={}
+              >
+                <MenuItem key={"STS"} value={"all-statuses"}>
+                  <ListItemText primary={"Todos"} />
                   <Checkbox
-                    checked={selectedStatuses.includes(filSta.description)}
+                    checked={selectedStatuses.includes("all-statuses")}
                     className="checkbox-filter"
+                    onChange={handleSelectAllStatuses}
                   />
                 </MenuItem>
-              ))}
-            </Select>
-            <Button
-              id="openStarDate"
-              onClick={() => setOpenStartDate(true)}
-              className="select-button"
-              disabled={!initComp}
-            >
-              {dateFrom
-                ? `Desde: ${moment
-                    .tz(dateFrom, "America/Argentina/Buenos_Aires")
-                    .format("DD/MM/YYYY")}`
-                : "Fecha Desde"}
-              {iconFilter()}
-            </Button>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                value={dateFrom}
-                onChange={(newDate) => setDateFrom(newDate)}
-                onClose={() => setOpenStartDate(false)}
-                open={openStartDate}
-                renderInput={() => <></>}
-                PopperProps={{
-                  anchorEl: document.getElementById("openStarDate"),
-                }}
-              />
-            </LocalizationProvider>
-            <Button
-              id="openEndDate"
-              onClick={() => setOpenEndDate(true)}
-              className="select-button"
-              disabled={!initComp}
-            >
-              {dateTo
-                ? `Hasta: ${moment
-                    .tz(dateTo, "America/Argentina/Buenos_Aires")
-                    .format("DD/MM/YYYY")}`
-                : "Fecha Hasta"}
-              {iconFilter()}
-            </Button>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-                value={dateTo}
-                onChange={(newDate) => setDateTo(newDate)}
-                onClose={() => setOpenEndDate(false)}
-                open={openEndDate}
-                renderInput={() => <></>}
-                PopperProps={{
-                  anchorEl: document.getElementById("openEndDate"),
-                }}
-              />
-            </LocalizationProvider>
-            <Button
-              id="openStarDate"
-              onClick={handleApply}
-              variant="contained"
-              disableElevation
-              className="select-button select-button-apply"
-              disabled={!initComp}
-            >
-              Aplicar
-            </Button>
+                <Divider />
+                {filterStatuses.map((filSta) => (
+                  <MenuItem key={filSta.code} value={filSta.description}>
+                    <ListItemText primary={filSta.description} />
+                    <Checkbox
+                      checked={selectedStatuses.includes(filSta.description)}
+                      className="checkbox-filter"
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+              <Button
+                id="openStarDate"
+                onClick={() => setOpenStartDate(true)}
+                className="select-button"
+                disabled={!initComp}
+              >
+                {dateFrom
+                  ? `Desde: ${moment
+                      .tz(dateFrom, "America/Argentina/Buenos_Aires")
+                      .format("DD/MM/YYYY")}`
+                  : "Fecha Desde"}
+                {iconFilter()}
+              </Button>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  value={dateFrom}
+                  onChange={(newDate) => setDateFrom(newDate)}
+                  onClose={() => setOpenStartDate(false)}
+                  open={openStartDate}
+                  renderInput={() => <></>}
+                  PopperProps={{
+                    anchorEl: document.getElementById("openStarDate"),
+                  }}
+                />
+              </LocalizationProvider>
+              <Button
+                id="openEndDate"
+                onClick={() => setOpenEndDate(true)}
+                className="select-button"
+                disabled={!initComp}
+              >
+                {dateTo
+                  ? `Hasta: ${moment
+                      .tz(dateTo, "America/Argentina/Buenos_Aires")
+                      .format("DD/MM/YYYY")}`
+                  : "Fecha Hasta"}
+                {iconFilter()}
+              </Button>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  value={dateTo}
+                  onChange={(newDate) => setDateTo(newDate)}
+                  onClose={() => setOpenEndDate(false)}
+                  open={openEndDate}
+                  renderInput={() => <></>}
+                  PopperProps={{
+                    anchorEl: document.getElementById("openEndDate"),
+                  }}
+                />
+              </LocalizationProvider>
+            </Stack>
             <Stack
               flexDirection="row"
-              justifyContent="flex-end"
+              justifyContent="center"
               alignItems="center"
-              flex="1"
+              flexWrap="wrap"
+              gap="0.5rem"
             >
               <IconButton
                 className="icon-button"
-                aria-label="Estadisticas"
+                aria-label="Limpiar Filtros"
                 size="medium"
-                onClick={handleOpen}
+                onClick={resetFilters}
               >
-                <BarChartOutlinedIcon />
+                <RestartAltIcon />
               </IconButton>
-              <IconButton
-                className="icon-button"
-                aria-label="Descargar Excel"
-                size="medium"
-                onClick={handleDownloadExcel}
+              <Button
+                id="openStarDate"
+                onClick={handleApply}
+                variant="contained"
+                disableElevation
+                className="select-button select-button-apply"
+                disabled={!initComp}
               >
-                <FaRegFileExcel size="1.3rem" />
-              </IconButton>
+                Aplicar
+              </Button>
             </Stack>
           </Stack>
         </FormGroup>
         <Divider></Divider>
-        <Stack
-          direction="row"
-          gap="1rem"
-          flexWrap="wrap"
-          className="filters-footer"
-        >
-          <div>
-            Total de Operaciones:
-            <Chip label={total} size="small" className="chip-container" />
-          </div>
-          <div>
-            Total Recaudado:
-            <Chip
-              label={currencyFormat(getTotalAmount())}
-              size="small"
-              className="chip-container"
-            />
-          </div>
+        <Stack direction="row" className="filters-footer">
+          <Stack
+            flexDirection="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            gap="1rem"
+            flex="1"
+            flexWrap="wrap"
+          >
+            <div>
+              Total de Operaciones:
+              <Chip label={total} size="small" className="chip-container" />
+            </div>
+            <div>
+              Total Recaudado:
+              <Chip
+                label={currencyFormat(getTotalAmount())}
+                size="small"
+                className="chip-container"
+              />
+            </div>
+          </Stack>
+          <Stack
+            flexDirection="row"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <IconButton
+              className="icon-button"
+              aria-label="Estadisticas"
+              size="medium"
+              onClick={handleOpen}
+            >
+              <BarChartOutlinedIcon />
+            </IconButton>
+            <IconButton
+              className="icon-button"
+              aria-label="Descargar Excel"
+              size="medium"
+              onClick={handleDownloadExcel}
+            >
+              <FaRegFileExcel size="1.3rem" />
+            </IconButton>
+          </Stack>
         </Stack>
         <PaymentFilterModal open={open} handleClose={handleClose} />
       </Paper>
